75a201fafdfd993f16d4ff9b998cd984e4214547
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-16 18:52:49 +0100

    moved game logic out of controller and into game class

--- controller.rb
+++ controller.rb
@@ -3,12 +3,6 @@ require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 
 get '/:shoot1/:shoot2' do
-  case
-  when params[:shoot1] == "rock" && params[:shoot2] == "scissors" || params[:shoot1] == "scissors" && params[:shoot2] == "rock"
-    return "Rock smashes scissors!"
-  when params[:shoot1] == "paper" && params[:shoot2] == "rock" || params[:shoot1] == "rock" && params[:shoot2] == "paper"
-    return "Paper covers rock!"
-  when params[:shoot1] == "paper" && params[:shoot2] == "scissors" || params[:shoot1] == "scissors" && params[:shoot2] == "paper"
-    return "Scissors cut paper!"
-  end
+  game = Game.new(params[:shoot1], params[:shoot2])
+  game.shoot()
 end
--- models/game.rb
+++ models/game.rb
@@ -1,9 +1,19 @@
-# class Game
-#   def initialize(player_1, player_2)
-#     @player_1 = player_1
-#     @player_2 = player_2
-#   end
-#
-#
-#
-# end
+class Game
+
+  def initialize(shoot1, shoot2)
+    @shoot1 = shoot1
+    @shoot2 = shoot2
+  end
+
+  def shoot
+    case
+    when @shoot1 == "rock" && @shoot2 == "scissors" || @shoot1 == "scissors" &&  @shoot2 == "rock"
+      return "Rock smashes scissors!"
+    when @shoot1 == "paper" &&  @shoot2 == "rock" || @shoot1 == "rock" && @shoot2 == "paper"
+      return "Paper covers rock!"
+    when @shoot1 == "paper" && @shoot2 == "scissors" || @shoot1 == "scissors" && @shoot2 == "paper"
+      return "Scissors cut paper!"
+    end
+  end
+
+end
