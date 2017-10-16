e77eb6888847e7ede1d1981bdf8fc9dffe500c02
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-16 20:05:03 +0100

    home/welcome page added

--- controller.rb
+++ controller.rb
@@ -2,6 +2,10 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 
+get '/' do
+  erb(:home)
+end
+
 get '/:shoot1/:shoot2' do
   game = Game.new(params[:shoot1], params[:shoot2])
   @winner = game.shoot()
